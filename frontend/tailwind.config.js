8818b5d81b34cad43a44396e8a6e791cbe2ae98c
tosta <tobias.stach@gmail.com>
2023-11-06 00:36:14 +0100

    tailwind colors added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {}
-	},
-	plugins: []
-};
+		extend: {
+		  colors: {
+			"primary": {
+			  100: "#F7E480",
+			  200: "#F3D640",
+			  300: "#EEC800",
+			  400: "#B39600",
+			  500: "#776400",
+			},
+			"secondary": {
+			  100: "#080808",
+			  200: "#040404",
+			  300: "#000000"
+			},
+			"main-transparent":{
+			  100: "rgba(0, 0, 0, 0.35)",
+			  200: "rgba(0, 0, 0, 0.55)"
+			}
+		  }
+		},
+	  },
+	  plugins: [],
+}
