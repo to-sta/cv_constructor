10e307e84b1a09dc990169cfa48bc2bdc8faefbc
tosta <tobias.stach@gmail.com>
2023-11-11 16:01:22 +0100

    frontend config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,25 +3,33 @@ export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		extend: {
-		  colors: {
-			"primary": {
-			  100: "#F7E480",
-			  200: "#F3D640",
-			  300: "#EEC800",
-			  400: "#B39600",
-			  500: "#776400",
+			colors: {
+				primary: {
+					100: "#F7E480",
+					200: "#F3D640",
+					300: "#EEC800",
+					400: "#B39600",
+					500: "#776400"
+				},
+				secondary: {
+					100: "#080808",
+					200: "#040404",
+					300: "#000000"
+				},
+				"main-transparent": {
+					100: "rgba(0, 0, 0, 0.35)",
+					200: "rgba(0, 0, 0, 0.55)"
+				},
+				error: {
+					100: "#D14D58",
+					200: "#CC2828",
+					300: "#991E1E"
+				}
 			},
-			"secondary": {
-			  100: "#080808",
-			  200: "#040404",
-			  300: "#000000"
-			},
-			"main-transparent":{
-			  100: "rgba(0, 0, 0, 0.35)",
-			  200: "rgba(0, 0, 0, 0.55)"
+			fontFamily: {
+				oswald: ["Oswald", "sans-serif"]
 			}
-		  }
-		},
-	  },
-	  plugins: [],
-}
+		}
+	},
+	plugins: []
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// envDir causes a restart in the docker setup and needs to be fixed
+	envDir: "../",
+	server: {
+		host: "0.0.0.0"
+	}
 });
