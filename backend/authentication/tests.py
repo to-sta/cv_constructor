3c1158cee91fb78a27c7dab6e8ab476f95a0ed40
tosta <tobias.stach@gmail.com>
2023-11-11 16:10:46 +0100

    helper test with requests deleted

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,10 +1,1 @@
 from django.test import TestCase
-import requests as rq
-
-# Create your tests here.
-payload = {
-    "email": "test@gmail.com",
-    "password": "MyPassword"
-}
-response = rq.post("http://127.0.0.1:8000/api/user/login/", data=payload)
-print(response.status_code, response.json())
