2c19176b1ac784c8091f8e9b1bf70e327be78573
tosta <tobias.stach@gmail.com>
2023-11-28 11:21:48 +0100

    Update API_ROOT variable

--- frontend/src/lib/dotenv/dotenv.ts
+++ frontend/src/lib/dotenv/dotenv.ts
@@ -1,5 +1,5 @@
 import type { Variables } from "$lib/interfaces/variables";
 
-const API_ROOT: string = (import.meta.env.DEV = import.meta.env.VITE_API_ROOT);
+const API_ROOT: string = import.meta.env.VITE_API_ROOT;
 
 export const variables: Variables = { API_ROOT: API_ROOT };
